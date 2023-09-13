86eeac5ecdbe486acb47a9ca6904e8b766f5c031
Noah Zinn <noahwzinn@gmail.com>
2023-09-13 16:43:42 -0500

    Initial commit

---
+++ assignment3.scm
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S guile -s
+!#
+
+
+
+
+(define (acc op sequence final-result)
+  (if (eq? sequence '())
+      final-result
+      (op (car sequence) (acc op (cdr sequence) final-result))))
+
+(define (adder-acc sequence)
+  (acc + sequence 0))
+
