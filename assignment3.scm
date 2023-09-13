5cfd7b912cfe878b4fa73215f8efb7df23eb2add
Noah Zinn <noahwzinn@gmail.com>
2023-09-13 18:51:44 -0500

    Created main function

--- assignment3.scm
+++ assignment3.scm
@@ -1,8 +1,13 @@
-#!/usr/bin/env -S guile -s
+#!/usr/bin/env -S guile -e main -s
 !#
 
 
+(use-modules (ice-9 rdelim))
 
+
+
+(define (get-input)
+  (map string->number (string-split (read-line) #\space)))
 
 (define (acc op sequence final-result)
   (if (eq? sequence '())
@@ -14,3 +19,12 @@
 
 (define (product-acc sequence)
   (acc * sequence 1))
+
+
+(define (main args)
+  (format #t "Enter a sequence of numbers: \n")
+  (let ((nums (get-input)))
+    (format #t "Sum: ~a\n" (adder-acc nums))
+    (format #t "Product: ~a\n" (product-acc nums))))
+
+
